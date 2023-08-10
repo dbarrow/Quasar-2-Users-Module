d6bca2495d231041368e0632cf702736b9248dbd
Darryl Barrow <darryl@traversepoint.com>
2023-08-10 17:39:46 -0400

    Overide

--- __tests__/UserStore.test.js
+++ __tests__/UserStore.test.js
@@ -5,9 +5,6 @@ import { assert, expectTypeOf } from "vitest";
 
 describe("User Store", () => {
   beforeEach(() => {
-    // creates a fresh pinia and make it active so it's automatically picked
-    // up by any useStore() call without having to pass it to it:
-    // `useStore(pinia)`
     setActivePinia(createPinia());
   });
 
